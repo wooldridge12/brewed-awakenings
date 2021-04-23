9499a290fc7c0fa17a34af9ec8d71233e68835bf
wooldridge12 <wooldridgemikey@gmail.com>
2021-04-23 14:04:46 -0400

    more debugging

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,21 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} cost $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
 const products = getProducts()
 
 export const Products = () => {
